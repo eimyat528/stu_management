ec507ad2a94d17ced8207dcfeb04382227f190ec
Ei Myat <74184859+eimyat528@users.noreply.github.com>
2024-04-01 17:32:59 +0630

    stcb

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BatchController;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\TeacherController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +18,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layout');
 });
+Route::resource('/students', StudentController::class);
+Route::resource('/teachers', TeacherController::class);
+Route::resource('/courses', CourseController::class);
+Route::resource('/batches', BatchController::class);
+
